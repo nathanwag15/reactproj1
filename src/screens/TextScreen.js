774364f517d7dec914fc7a72a888c50d74af66c9
Nate Wagstaff <natewagstaff21@gmail.com>
2022-04-08 20:06:15 -0600

    text screen output added

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { State } from 'react-native-gesture-handler';
 
 const TextScreen = () => {
+    const [name, setName] = useState('')
     return(
         <View>
             <TextInput 
                 style={styles.input}
                 autoCapitalize='none'
                 autoCorrect={false}
+                value={name}
+                onChangeText={(newValue) => setName(newValue)}
             />
+
+            <Text>My name is {name}</Text>
         </View>
     );    
 };
@@ -18,7 +24,7 @@ const styles = StyleSheet.create({
         margin: 15,
         borderColor: 'black',
         borderWidth: 1,
-        borderRadius: 10
+        borderRadius: 5
     }
 });
 
